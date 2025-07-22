27379a84d5198d719fc2cd566caa41e6ce97f832
Alvimz <gg_alvim@hotmail.com>
2025-07-22 11:34:00 -0300

    Adiciona campo pattern RepositoryClassMethod

--- src/main/java/com/alvim/repository/RepositoryClassMethod.java
+++ src/main/java/com/alvim/repository/RepositoryClassMethod.java
@@ -1,14 +1,18 @@
 package com.alvim.repository;
 
 import java.lang.reflect.Method;
+import java.lang.reflect.Parameter;
+import java.util.regex.Pattern;
 
 public class RepositoryClassMethod {
     private final Class<?> clazz;
     private final Method method;
+    private final Pattern pattern;
 
-    public RepositoryClassMethod(Class<?> clazz, Method method) {
+    public RepositoryClassMethod(Class<?> clazz, Method method, Pattern pattern) {
         this.clazz = clazz;
         this.method = method;
+        this.pattern = pattern;
     }
 
     public Class<?> getClazz() {
@@ -17,5 +21,9 @@ public class RepositoryClassMethod {
 
     public Method getMethod() {
         return method;
+    }
+
+    public Pattern getPattern() {
+        return pattern;
     }
 }
