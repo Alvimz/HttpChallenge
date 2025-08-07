3d1706b56a89408cfc9472ad562c7f3f4e93d30f
Alvimz <gg_alvim@hotmail.com>
2025-08-07 15:50:58 -0300

    Apaga arquivo que não funcoinava

--- src/main/java/com/alvim/endpoints/Vnf.java
+++ src/main/java/com/alvim/endpoints/Vnf.java
@@ -4,16 +4,12 @@ import com.alvim.annotations.EndPointMethod;
 import com.alvim.annotations.Endpoint;
 import com.alvim.boot.FoldersVolume;
 import com.alvim.http.HttpMethodRequest;
-import com.alvim.repository.LoggerContainer;
 import com.github.dockerjava.api.DockerClient;
-import com.github.dockerjava.api.async.ResultCallback;
 import com.github.dockerjava.api.command.CreateContainerResponse;
 
-import com.github.dockerjava.api.command.LogContainerCmd;
 import com.github.dockerjava.api.model.HostConfig;
 
 import com.github.dockerjava.core.DockerClientBuilder;
-import com.github.dockerjava.core.command.LogContainerCmdImpl;
 
 import java.util.UUID;
 
@@ -31,8 +27,6 @@ public class Vnf {
         //o env TASK <- Dita qual vai ser a ação! Adicionar no ansible e no Java!
         String containerId = container.getId(); //pega o id do container!
         dockerClient.startContainerCmd(containerId).exec();
-        LoggerContainer loggerContainer = new LoggerContainer(containerId);
-        dockerClient.logContainerCmd(containerId).withStdErr(true).withStdOut(true).withTimestamps(true).exec(loggerContainer);
 
         //TODO instanciar o loggerContainer
         System.out.println("Container iniciado: " + containerId);
@@ -42,16 +36,12 @@ public class Vnf {
 
 
         TODO: criação da imagem do DOckerfile!
-        TODO: adicionar keep alive and ping na vnf.
+        TODO: adicionar keep alive and ping na vnf. Ansible uri!
 
          */
     }
 
-    @EndPointMethod(path = "/{containerId}",method = HttpMethodRequest.GET)
-    public LogContainerCmd consultContainer(String containerId){
-        LogContainerCmd logContainerCmd = dockerClient.logContainerCmd(containerId);
-        return logContainerCmd;
-    }
+
 
     public UUID getId() {
         return id;
