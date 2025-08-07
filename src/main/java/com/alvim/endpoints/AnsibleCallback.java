096a7cd4413ae0eb15272bb4b47629d7629b97b3
Alvimz <gg_alvim@hotmail.com>
2025-08-07 17:05:29 -0300

    Teste1

--- src/main/java/com/alvim/endpoints/AnsibleCallback.java
+++ src/main/java/com/alvim/endpoints/AnsibleCallback.java
@@ -6,10 +6,19 @@ import com.alvim.http.HttpMethodRequest;
 
 @Endpoint(path = "/ansible")
 public class AnsibleCallback {
+    private String status;
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
 
     @EndPointMethod(path = "/callback", method = HttpMethodRequest.POST)
-    public void handleCallback(String body){
-        System.out.println("Recebido :" + body);
+    public void handleCallback(String status){
+        System.out.println("Recebido :" + status);
 
     }
 }
